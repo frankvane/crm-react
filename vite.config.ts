8ffb845b1080a7981d35ae977a13a1ccbd0a3944
vane <13525442@qq.com>
2025-05-03 21:18:03 +0800

    feat: vite-plugin-cdn-import的打包抽离应用

--- vite.config.ts
+++ vite.config.ts
@@ -1,51 +1,16 @@
-import { createHtmlPlugin } from "vite-plugin-html";
+import cdnImport from "vite-plugin-cdn-import";
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
-
-const isProd = process.env.NODE_ENV === "production";
-
-const cdnScripts = [
-  '<script src="https://cdn.jsdelivr.net/npm/react@18/umd/react.production.min.js"></script>',
-  '<script src="https://cdn.jsdelivr.net/npm/react-dom@18/umd/react-dom.production.min.js"></script>',
-  '<script src="https://cdn.jsdelivr.net/npm/lodash@4/lodash.min.js"></script>',
-  '<script src="https://cdn.jsdelivr.net/npm/axios@1/dist/axios.min.js"></script>',
-].join("\n");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    createHtmlPlugin({
-      inject: {
-        data: {
-          cdn: isProd ? cdnScripts : "",
-        },
-      },
+    cdnImport({
+      modules: ["react", "react-dom", "lodash", "axios", "dayjs", "antd"],
     }),
   ],
-  ...(isProd
-    ? {
-        optimizeDeps: {
-          exclude: ["react", "react-dom", "lodash", "axios"],
-          include: ["dayjs"],
-        },
-        build: {
-          rollupOptions: {
-            external: ["react", "react-dom", "lodash", "axios"],
-            output: {
-              globals: {
-                react: "React",
-                "react-dom": "ReactDOM",
-                lodash: "_",
-                axios: "axios",
-              },
-              format: "iife",
-            },
-          },
-        },
-      }
-    : {}),
   // 别名、后缀
   resolve: {
     alias: {
