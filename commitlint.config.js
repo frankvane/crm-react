b61d219e012de4c8deca5d04fd111dbd819f3edb
MyBrainstorming <15617186529@163.com>
2025-05-22 14:55:49 +0800

    fix: 修复了eslint与pertier冲突问题，并修复Husky和commitlint不工作问题。

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,28 +1,28 @@
 export default {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "type-enum": [
-      2,
-      "always", 
-      [
-        "feat", // 新功能
-        "fix", // 修复
-        "docs", // 文档
-        "style", // 样式
-        "refactor", // 重构
-        "perf", // 性能优化
-        "test", // 测试
-        "build", // 构建
-        "ci", // CI配置
-        "chore", // 杂务
-        "revert" // 回退
-      ]
-    ],
-    "type-case": [2, "always", "lower"],
-    "type-empty": [2, "never"],
-    "subject-empty": [2, "never"],
-    "subject-full-stop": [2, "never", "."],
-    "subject-case": [0],
-    "header-max-length": [2, "always", 72]
-  }
-}; 
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			[
+				"feat", // 新功能
+				"fix", // 修复
+				"docs", // 文档
+				"style", // 样式
+				"refactor", // 重构
+				"perf", // 性能优化
+				"test", // 测试
+				"build", // 构建
+				"ci", // CI配置
+				"chore", // 杂务
+				"revert", // 回退
+			],
+		],
+		"type-case": [2, "always", "lowercase"],
+		"type-empty": [2, "never"],
+		"subject-empty": [2, "never"],
+		"subject-full-stop": [2, "never", "."],
+		"subject-case": [0],
+		"header-max-length": [2, "always", 72],
+	},
+};
